9f5b8b51321451389f847817924f6a400edaa868
VitorDxD <vitornunes2747@gmail.com>
2025-01-22 22:15:49 -0300

    add migration createUserTable

---
+++ migrations/20250123002340-createUserTable.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'moderator', 'user'),
+        defaultValue: 'user',
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
